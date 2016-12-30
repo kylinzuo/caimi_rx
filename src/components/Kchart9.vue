d8ac9a09ebfc94e4d996280dbf97d9118abd578f
zuokylin <zuokylin@gmail.com>
2016-12-30 15:56:01 +0800

    增加九宫格页面

--- src/components/Kchart9.vue
+++ src/components/Kchart9.vue
@@ -10,88 +10,90 @@
 </template>
 
 <script>
-// import RenderKChart from '../charts/k.chart'
-// import { getKchartService } from '../service/stockService'
-// let filterData = []
-// let newData = []
+import RenderKChart from '../charts/k.chart'
+import { getKchartService } from '../service/stockService'
+let filterData = {}
+let newData = {}
 export default {
   name: 'hello',
   data () {
     return {
       msg: 'Welcome to Your Vue.js App',
       lists: ['MA', 'VOL', 'VOL(MA)'],
-      kChart: null,
-      period: ['Sec15', 'Sec30', 'Min1', 'Min5', 'Min15', 'Min30', 'Min60', 'Day1', 'Day7', 'Day30']
+      kChart: {},
+      period: ['Sec15', 'Sec30', 'Min1', 'Min5', 'Min15', 'Min30', 'Min60', 'Day1', 'Day7']
+    }
+  },
+  mounted () {
+  //   // 000001.SS 000001.SZ 000625.SZ 002312.SZ {symbol, period, startTime, lastNPoints = 0}
+    this.period.forEach((d, i) => {
+      getKchartService({
+        symbol: '002312.SZ',
+        period: d
+      }, data => {
+        filterData[d] = []
+        newData[d] = []
+        filterData[d] = data.slice(0, 200)
+        newData[d] = data.slice(200)
+        let param = {
+          id: d, // 画布容器
+          title: '三态控股', // 股票名称
+          period: d, // 周期
+          lists: this.lists, // 指标列表
+          totalShares: 1378091733, // 总股本
+          data: filterData[d] // k线数据
+        }
+        let config = {
+          mode: 0, // 默认模式0 九宫格模式1
+          theme: 0, // 默认白色颜色主题0
+          scrollBar: false, // 是否显示底部滑动条
+          cursorInteract: false, // 是否允许光标交互
+          dragZoom: false, // 是否允许拖拽与缩放
+          settingBtn: false, // 是否显示设置按钮
+          title: false // 是否显示股票名称
+        }
+        this.tsChart[d] = new RenderKChart({
+          param: param,
+          config: config,
+          cb: this.cb})
+      })
+    })
+  },
+  methods: {
+    refreshChart () {
+      console.log('refreshChart')
+      if (newData.length > 0) {
+        filterData.push(newData.shift())
+      }
+      this.tsChart.refreshChart(filterData)
+    },
+    checkLists (val) {
+      let index = this.lists.indexOf(val)
+      if (index >= 0) {
+        this.lists.splice(index, 1)
+        if (val === 'VOL') {
+          if (this.lists.indexOf('MA(VOL)') >= 0) {
+            this.lists.splice(this.lists.indexOf('MA(VOL)'), 1)
+          }
+        }
+      } else {
+        this.lists.push(val)
+        if (val === 'MA(VOL)') {
+          if (this.lists.indexOf('VOL') < 0) {
+            this.lists.push('VOL')
+          }
+        }
+      }
+      this.tsChart.updateIndicators(Object.assign([], this.lists))
+    },
+    cb (data) {
+      console.log(data)
+      if (data.type === 'close') {
+        let index = this.lists.indexOf(data.data)
+        this.lists.splice(index, 1)
+      }
     }
   }
-  // mounted () {
-  //   // 000001.SS 000001.SZ 000625.SZ 002312.SZ {symbol, period, startTime, lastNPoints = 0}
-  //   period.forEach((d, i) => {
-  //     getKchartService({
-  //       symbol: '002312.SZ',
-  //       period: d
-  //     }, data => {
-  //       filterData = data.slice(0, 200)
-  //       newData = data.slice(200)
-  //       let param = {
-  //         id: 'kChartContainer', // 画布容器
-  //         title: '三态控股', // 股票名称
-  //         period: d, // 周期
-  //         lists: this.lists, // 指标列表
-  //         totalShares: 1378091733, // 总股本
-  //         data: filterData // k线数据
-  //       }
-  //       let config = {
-  //         mode: 0, // 默认模式0 九宫格模式1
-  //         theme: 0, // 默认白色颜色主题0
-  //         scrollBar: false, // 是否显示底部滑动条
-  //         cursorInteract: false, // 是否允许光标交互
-  //         dragZoom: false, // 是否允许拖拽与缩放
-  //         settingBtn: false, // 是否显示设置按钮
-  //         title: false // 是否显示股票名称
-  //       }
-  //       this.tsChart = new RenderKChart({
-  //         param: param,
-  //         config: config,
-  //         cb: this.cb})
-  //     })
-  //   })
-  // },
-  // methods: {
-  //   refreshChart () {
-  //     console.log('refreshChart')
-  //     if (newData.length > 0) {
-  //       filterData.push(newData.shift())
-  //     }
-  //     this.tsChart.refreshChart(filterData)
-  //   },
-  //   checkLists (val) {
-  //     let index = this.lists.indexOf(val)
-  //     if (index >= 0) {
-  //       this.lists.splice(index, 1)
-  //       if (val === 'VOL') {
-  //         if (this.lists.indexOf('MA(VOL)') >= 0) {
-  //           this.lists.splice(this.lists.indexOf('MA(VOL)'), 1)
-  //         }
-  //       }
-  //     } else {
-  //       this.lists.push(val)
-  //       if (val === 'MA(VOL)') {
-  //         if (this.lists.indexOf('VOL') < 0) {
-  //           this.lists.push('VOL')
-  //         }
-  //       }
-  //     }
-  //     this.tsChart.updateIndicators(Object.assign([], this.lists))
-  //   },
-  //   cb (data) {
-  //     console.log(data)
-  //     if (data.type === 'close') {
-  //       let index = this.lists.indexOf(data.data)
-  //       this.lists.splice(index, 1)
-  //     }
-  //   }
-  // }
 }
 </script>
 
@@ -104,9 +106,24 @@ export default {
   border: 1px solid #ccc;
   background-color: #fff;
 }
+ul{
+  margin: 0;
+  padding: 0;
+  width: 100%;
+  height: 100%;
+}
 ul li {
   margin: 0;
   padding: 0;
+  list-style: none;
+  width: 33%;
+  height: 33%;
+  border: 1px solid #eee;
+  float: left;
+}
+ul li div{
+  width: 100%;
+  height: 100%;
 }
 
 </style>
